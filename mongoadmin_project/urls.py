61f1034adbf79e560f3184622acf69a88a9156c4
gacanonf <35529703+gacanonf@users.noreply.github.com>
2018-01-17 13:59:14 +0100

    Update urls.py

--- mongoadmin_project/urls.py
+++ mongoadmin_project/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^accounts/logout/', 'django.contrib.auth.views.logout',
